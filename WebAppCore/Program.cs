aee9b8a4baec7544b0851334b6c2ac979ac317bb
Vagrant <anton.evtikhov@gmail.com>
2018-06-25 17:59:21 +0300

    test Pr12

--- WebAppCore/Program.cs
+++ WebAppCore/Program.cs
@@ -24,3 +24,18 @@ namespace WebAppCore
 }
 
 
+namespace WebAppCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
